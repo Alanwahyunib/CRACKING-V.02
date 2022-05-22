b3ee8433cce521dc825db1dcd45a1a1c4faa0da5
Mr. ALONE <101225561+KILLER-BOY-202@users.noreply.github.com>
2022-05-23 00:54:18 +0300

    Update Crack.py

--- Crack.py
+++ Crack.py
@@ -2,7 +2,7 @@ import os, platform
 os.system('git pull')
 bit = platform.architecture()[0]
 if bit == '64bit':
-    from killerp import menu
-    menu()
+    from killerp import killerboy
+    killerboy()
 elif bit == '32bit':
     exit('\x1b[1;91m\n\tOpps Your Device Not Supported')
